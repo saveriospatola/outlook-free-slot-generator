fff6ee46267d32a3f7febb5a06bfda4bb92fd4ce
Saverio <saveriospatola@users.noreply.github.com>
2026-02-19 14:49:55 +0100

    Delete outlook-free-slot-generator-text.bat

--- outlook-free-slot-generator-text.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM --- Lancia lo script PowerShell ignorando temporaneamente la policy ---
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File ".\outlook-free-slot-generator-text.ps1"
